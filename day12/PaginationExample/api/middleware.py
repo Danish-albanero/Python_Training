5b9a66f51ddd66ed0fa2e31978950f311ce5eb12
Danish-albanero <90675558+Danish-albanero@users.noreply.github.com>
2021-09-30 15:43:52 +0530

    Delete day12/PaginationExample directory

--- day12/PaginationExample/api/middleware.py
+++
@@ -1,8 +0,0 @@
-class MyCustomMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        print("Source of request is : ", request.META['HTTP_USER_AGENT'])
-        return response
