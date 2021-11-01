b72bdedf6f8b090ef1c6c5be21cf548ac3d68b8d
Danish-albanero <90675558+Danish-albanero@users.noreply.github.com>
2021-11-01 18:15:28 +0530

    Delete platform-dataAnalyzer-release-v1.0.0 directory

--- platform-dataAnalyzer-release-v1.0.0/spark_jobs/join/handler.py
+++
@@ -1,9 +0,0 @@
-from . import s3_analyzer
-from . import pg_analyzer
-from . import oracle_analyzer
-
-join_analyzers = {
-    "s3": s3_analyzer,
-    "postgresql": pg_analyzer,
-    "oracle": oracle_analyzer,
-}
